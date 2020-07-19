b2179abf51e9162c6efe4be48b27c9ede99b5fd8
HermersonDev <ne.hermerson@gmail.com>
2020-07-18 21:46:29 -0300

    :lipstick: create global style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <Routes />
     </Provider>
   );
---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+ 
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap');
+
+*, *::before, *::after{
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #353940;
+  color: #fff;
+  font-family: 'Source Sans Pro', Arial, sans-serif;
+  text-rendering: optimizeLegibility !important;
+  --webkit-text-smothing: antialiased !important;
+}
+
+html, body, #root { height: 100%; }
+
+input, button {
+  font-family: 'Source Sans Pro', Arial, sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+`;
