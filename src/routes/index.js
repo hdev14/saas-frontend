9094c5267ed4afd9b29561455161d242f0ef68cd
HermersonDev <ne.hermerson@gmail.com>
2020-07-18 21:19:17 -0300

    :construction: create routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const Routes = () => (<h1>routes</h1>);
+const Main = lazy(() => import('../pages/Main'));
+const SignIn = lazy(() => import('../pages/Auth/SignIn'));
+const SignUp = lazy(() => import('../pages/Auth/SignUp'));
+
+const Loading = <div>loading...</div>;
+
+const Routes = () => (
+  <BrowserRouter>
+    <Suspense fallback={Loading}>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </Suspense>
+  </BrowserRouter>
+);
 
 export default Routes;
