82e0f967ba0ccde4179c4ba4b4eb2c7bc7a4bf87
HermersonDev <ne.hermerson@gmail.com>
2020-07-23 11:10:12 -0300

    :construction: fetch team projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { FiPlus } from 'react-icons/fi';
+import { toast } from 'react-toastify';
+
+import api from '../../services/api';
 
 import Button from '../../styles/components/Button';
 import { Container, Project } from './styles';
 
 const Projects = () => {
   const activeTeam = useSelector((state) => state.activeTeam.active);
+  const [projects, setProjects] = useState([]);
 
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await api.get('/projects');
+        setProjects(response.data);
+      } catch (err) {
+        toast.error('Não foi possível carregar os projetos.');
+      }
+    }
+
+    fetchProjects();
+  });
   return (
     <Container>
       {activeTeam ? (
@@ -23,9 +39,9 @@ const Projects = () => {
             </div>
           </header>
 
-          <Project>First Project</Project>
-          <Project>First Project</Project>
-          <Project>First Project</Project>
+          {projects.map((project) => (
+            <Project>{project.title}</Project>
+          ))}
         </>
       ) : (
         <h2>Selecione um time ou crie um novo!</h2>
